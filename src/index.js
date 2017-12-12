d43a99e84453f14311f2cc213188faccac8992a8
Eric Jinks <ericjinks@gmail.com>
2017-12-12 14:04:38 +1000

    App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class HelloMessage extends React.Component {
+class App extends React.Component {
   render() {
     return <div>Heldlo {this.props.name}</div>
   }
 }
 
 var mountNode = document.getElementById('app')
-ReactDOM.render(<HelloMessage name="World" />, mountNode)
+ReactDOM.render(<App name="World" />, mountNode)
